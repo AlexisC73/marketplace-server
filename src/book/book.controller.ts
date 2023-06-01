ec6a305dc620d5303e91889874375f052c9a55c7
Alexis Comte <alexis.comte73@icloud.com>
2023-06-01 16:04:35 +0200

    fix post request book

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -12,16 +12,10 @@ import { AddBookDTO } from './dto/add-book-dto';
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
-  @Get()
-  async test() {
-    return 'hello world';
-  }
-
   @Post()
   async addBook(@Body() addBookDTO: AddBookDTO) {
     try {
       await this.bookService.addBook(addBookDTO);
-      return 'The book is added';
     } catch (err) {
       throw new BadRequestException(err.message);
     }
