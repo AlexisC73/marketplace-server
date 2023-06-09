25aaccb0efefe6ecc7578975e252db857e477170
Alexis Comte <alexis.comte73@icloud.com>
2023-06-09 15:52:04 +0200

    changement de erreur de retour

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,5 @@
 import { PrismaService } from '@app/good-place/infrastructure/prisma/prisma.service';
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { SignInUserDTO } from './dto/signin-user.dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
@@ -20,12 +16,12 @@ export class AuthService {
       where: { email },
     });
     if (!fundUser) {
-      throw new NotFoundException('User not found');
+      throw new ForbiddenException();
     }
 
     const isPasswordValid = await bcrypt.compare(password, fundUser.password);
     if (!isPasswordValid) {
-      throw new BadRequestException('Invalid password');
+      throw new ForbiddenException();
     }
 
     const token = await this.jwtService.signAsync(
